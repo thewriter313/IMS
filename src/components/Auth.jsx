cb7b43dc9c836af4046ce0d4d49756171c823869
Umair Malik <umairmalik7777777@gmail.com>
2023-12-23 16:53:06 +0300

    Simple Auth page trial

---
+++ src/components/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+        <h1>Log In</h1>
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Password'/>
+        <button>Log In</button>
+    </div>
+  )
+}
+
+export default Auth
